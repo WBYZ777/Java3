d0c310358c8b2fe0c8bad2fd5d358da12f33da35
WBYZ777 <92857635+WBYZ777@users.noreply.github.com>
2023-06-08 11:55:19 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-
 public class Main {
-    public static void main() {
-    System.out.println("Hello");
+    public Main() {
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 }
